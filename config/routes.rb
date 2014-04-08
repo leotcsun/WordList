9e5352ec8e0946b21fd0e99eb8a4bc5f3b98b57d
Leo Sun <leo.tcsun@gmail.com>
2014-04-08 18:14:06 -0400

    modified route for the test page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ WordList::Application.routes.draw do
   resources :words
   resources :lists
 
-  get "/test/(:list_id)/(:order)", to: "words#test", as: "test"
+  get "/test", to: "words#test", as: "test"
   get "/edit_all", to: "words#edit_all", as: "edit_all"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
